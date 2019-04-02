25052f1236e520b6fdd4ebdc3b78a400462276e3
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2019-04-01 20:12:08 -0700

    user ID, form parsing

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/zikaeroh/strawrank
 go 1.12
 
 require (
-	github.com/davecgh/go-spew v1.1.1
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/google/go-cmp v0.2.0
 	github.com/gorilla/csrf v1.5.1
+	github.com/gorilla/securecookie v1.1.1
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/rs/xid v1.2.1
 	github.com/rs/zerolog v1.13.0
 	github.com/valyala/quicktemplate v1.0.2
 	golang.org/x/net v0.0.0-20190328230028-74de082e2cca // indirect
--- main.go
+++ main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"time"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/gorilla/csrf"
+	"github.com/gorilla/securecookie"
+	"github.com/rs/xid"
 	"github.com/rs/zerolog"
 	"github.com/zikaeroh/strawrank/internal/templates"
 )
 
 func main() {
+	secureKey := []byte("a-32-byte-long-key-goes-here")
+
 	logger := zerolog.New(zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) {
 		w.TimeFormat = time.RFC3339
 	})).With().Timestamp().Caller().Logger()
+
+	sc := securecookie.New(secureKey, nil)
 
 	r := chi.NewRouter()
 
@@ -32,7 +38,8 @@ func main() {
 	r.Use(requestLogger)
 	r.Use(recoverer)
 
-	r.Use(csrf.Protect([]byte("a-32-byte-long-key-goes-here"),
+	r.Use(csrf.Protect(
+		secureKey,
 		csrf.Secure(false), // TODO: debug flag
 	))
 
@@ -40,11 +47,40 @@ func main() {
 		w.Write([]byte("hi"))
 	})
 
+	r.Get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	})
+
 	r.Route("/{id}", func(r chi.Router) {
 		fn := func(w http.ResponseWriter, r *http.Request) {
+			logger := zerolog.Ctx(r.Context())
+
+			user, err := getSetUserID(sc, w, r)
+			if err != nil {
+				logger.Debug().Err(err).Msg("failed to get user ID")
+				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+				return
+			}
+
+			// Hack to deal with zerolog's bad API
+			tmpLogger := logger.With().Str("user", user.String()).Logger()
+			logger = &tmpLogger
+
 			if r.Method == http.MethodPost {
-				r.ParseForm()
-				spew.Dump(r.Form)
+				votesStr := r.FormValue("votes")
+				var votes []int
+
+				if err := json.Unmarshal([]byte(votesStr), &votes); err != nil {
+					// TODO: Do someting in the UI instead.
+					http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+					return
+				}
+
+				logger.Debug().Ints("votes", votes).Msg("posted vote")
+
+				// Post/Redirect/Get
+				http.Redirect(w, r, r.URL.String()+"/r", http.StatusSeeOther)
+				return
 			}
 
 			templates.WritePageTemplate(w, &templates.VotePage{
@@ -115,4 +151,44 @@ func recoverer(next http.Handler) http.Handler {
 	}
 
 	return http.HandlerFunc(fn)
+}
+
+func getSetUserID(s securecookie.Codec, w http.ResponseWriter, r *http.Request) (xid.ID, error) {
+	const cookieName = "user-id"
+
+	// Using a struct with the data inside for forwards compatibility.
+	cookie := struct {
+		XID xid.ID `json:"xid"`
+	}{}
+
+	c, err := r.Cookie(cookieName)
+
+	if err == nil {
+		if err := s.Decode(cookieName, c.Value, &cookie); err != nil {
+			return xid.NilID(), err
+		}
+
+		if !cookie.XID.IsNil() {
+			return cookie.XID, nil
+		}
+	} else if err != http.ErrNoCookie {
+		return xid.NilID(), err
+	}
+
+	cookie.XID = xid.New()
+
+	encoded, err := s.Encode(cookieName, cookie)
+	if err != nil {
+		return xid.NilID(), err
+	}
+
+	c = &http.Cookie{
+		Name:     cookieName,
+		Value:    encoded,
+		Path:     "/",
+		HttpOnly: true,
+	}
+	http.SetCookie(w, c)
+
+	return cookie.XID, nil
 }
